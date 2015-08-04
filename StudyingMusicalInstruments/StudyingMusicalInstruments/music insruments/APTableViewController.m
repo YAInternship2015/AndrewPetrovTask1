c31a8ac23e67255c7834139a1afd8ec1d7396c6f
“Andrew <andrew.petrov.dev@gmail.com>
2015-08-04 22:56:42 +0300

    return APTableViewController.h and move data source there

---
+++ StudyingMusicalInstruments/StudyingMusicalInstruments/music insruments/APTableViewController.m
@@ -0,0 +1,22 @@
+//
+//  APTableViewController.m
+//  StudyingMusicalInstruments
+//
+//  Created by Андрей on 8/3/15.
+//  Copyright (c) 2015 Андрей. All rights reserved.
+//
+
+
+@implementation APTableViewController 
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+@end
