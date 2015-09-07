6cde951b84719e96901a69fc1ab4fdfeb7e8d7e6
“Andrew <andrew.petrov.dev@gmail.com>
2015-09-07 23:00:16 +0300

    add core data stack without magical record

--- StudyingMusicalInstruments/AppDelegate.h
+++ StudyingMusicalInstruments/AppDelegate.h
@@ -14,5 +14,13 @@
 
 @property (nonatomic, strong) UIWindow *window;
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
 @end
 
