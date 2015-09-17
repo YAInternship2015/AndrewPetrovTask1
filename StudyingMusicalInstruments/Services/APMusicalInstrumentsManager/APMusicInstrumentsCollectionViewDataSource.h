62efe5fdec83326f6aedf2fd28a49ca4a6b9fc0c
“Andrew <andrew.petrov.dev@gmail.com>
2015-09-17 22:31:44 +0300

    add APMusicInstrumentsCollectionViewDataSource

---
+++ StudyingMusicalInstruments/Services/APMusicalInstrumentsManager/APMusicInstrumentsCollectionViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  APMusicInstrumentsCollectionViewDataSource.h
+//  StudyingMusicalInstruments
+//
+//  Created by Андрей on 9/17/15.
+//  Copyright © 2015 Андрей. All rights reserved.
+//
+
+#import "APMusicInstrumentsDataSource.h"
+
+@interface APMusicInstrumentsCollectionViewDataSource : APMusicInstrumentsDataSource
+
+@end
---
+++ StudyingMusicalInstruments/Services/APMusicalInstrumentsManager/APMusicInstrumentsCollectionViewDataSource.m
@@ -0,0 +1,13 @@
+//
+//  APMusicInstrumentsCollectionViewDataSource.m
+//  StudyingMusicalInstruments
+//
+//  Created by Андрей on 9/17/15.
+//  Copyright © 2015 Андрей. All rights reserved.
+//
+
+#import "APMusicInstrumentsCollectionViewDataSource.h"
+
+@implementation APMusicInstrumentsCollectionViewDataSource
+
+@end
